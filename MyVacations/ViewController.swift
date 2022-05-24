3db34a2eb83dc3c0b956a5854edc6941786ed2b7
a.Lyakhov <a.lyakhov@intellectika.ru>
2022-05-24 14:51:40 +0300

    Initial Commit

---
+++ MyVacations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyVacations
+//
+//  Created by Aleksandr Lyakhov on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
